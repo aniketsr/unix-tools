d81348d85f105348ed69c8d5d0372be37c8889b7
ANU <aniket@sabkuch.com>
2014-01-13 10:55:09 +0530

    Guard clause added to cut for input validations

--- src/aniketsr/unixtools/tail/Tail.java
+++ src/aniketsr/unixtools/tail/Tail.java
@@ -6,7 +6,6 @@ import java.io.File;
 
 public class Tail {
     TailOperations tail;
-
     public Tail(String fileName) {
         MyFile file = new MyFile();
         String text = file.readFile(fileName);
@@ -18,6 +17,10 @@ public class Tail {
     }
 
     public static void main(String[] args) {
+        if (args.length<1){
+            System.err.println("Atleast filename is needed");
+            System.exit(1);
+        }
         try {
             Tail cli = new Tail(args[0]);
             TailOperations head = cli.getTail();
