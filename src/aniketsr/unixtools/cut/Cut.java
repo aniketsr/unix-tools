8408ffd728f7d90efcc726a650fcdec5386e0774
ANU <aniket@sabkuch.com>
2014-01-13 10:29:54 +0530

    Guard claus added to cut and code refactored

--- src/aniketsr/unixtools/cut/Cut.java
+++ src/aniketsr/unixtools/cut/Cut.java
@@ -3,26 +3,35 @@ import aniketsr.FileSystem.MyFile;
 
 public class Cut {
     public static void main(String args[]) {
-        MyFile file = new MyFile();
-        CutOperations cut = new CutOperations();
-        String result="";
-        String separator = " ";
-        int column = 0 ;
-
-        for (int i = 0; i < args.length; i++) {
-            if(args[i].startsWith("-d")){
-                separator = args[i].substring(2);
-            }
-            else if(args[i].startsWith("-f")) {
-                column = Integer.parseInt(args[i].substring(2));
-            }
-            else if(args[i].endsWith(".txt")) {
-                result = file.readFile(args[i]);
-            }
-            else{
-                result = args[i];
-            }
+        if (args.length<2){
+            System.err.println("Filename and field number is necessary");
+            System.exit(1);
         }
-        System.out.println(cut.getSpecificFeild(column, result, separator));
+        try {
+            MyFile file = new MyFile();
+            CutOperations cut = new CutOperations();
+            String result="";
+            String separator = " ";
+            int column = 0 ;
+
+            for (int i = 0; i < args.length; i++) {
+                if(args[i].startsWith("-d")){
+                    separator = args[i].substring(2);
+                }
+                else if(args[i].startsWith("-f")) {
+                    column = Integer.parseInt(args[i].substring(2));
+                }
+                else if(args[i].endsWith(".txt")) {
+                    result = file.readFile(args[i]);
+                }
+                else{
+                    result = args[i];
+                }
+            }
+            System.out.println(cut.getSpecificFeild(column, result, separator));
+        }catch (Exception e){
+            System.out.println("Something went wrong " + e);
+        }
+
     }
 }
